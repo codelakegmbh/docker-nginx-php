05699a330777afa6db59a744c22741cd4367c859
Jöern Neumeyer <joernneumeyer@protonmail.ch>
2019-04-08 23:38:23 +0200

    added automatic certificate generation

--- startup.sh
+++ startup.sh
@@ -1,5 +1,24 @@
 set -e
 
+if [[ ! -f /srv/cert/fullchain.pem ]] || [[ ! -f /srv/cert/privkey.pem ]]
+then
+  if [[ -f /srv/cert/fullchain.pem ]]
+  then
+    rm /srv/cert/fullchain.pem
+  fi
+
+  if [[ -f /srv/cert/privkey.pem ]]
+  then
+    rm /srv/cert/privkey.pem
+  fi
+
+
+  openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /srv/cert/privkey.pem \
+    -out /srv/cert/fullchain.pem -outform PEM \
+    -subj "/C=DE/ST=Berlin/L=Berlin/O=Some Company/OU=IT Department/CN=example.com"
+fi
+
 nginx
 php-fpm7
 
